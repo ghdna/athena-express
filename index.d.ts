6e3de93d254bae540ff0604401dc6651622ed168
allen-munsch <enderplux@protonmail.com>
2019-09-13 11:19:45 -0500

    Adds an initial typings for using typescript with athena-express

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+declare module 'athena-express' {
+    import * as aws from 'aws-sdk';
+    interface ConnectionConfigInterface {
+        aws: typeof aws;
+        s3: string;
+        getStats: boolean;
+    }
+
+    type GenericObject = {
+        [names: string]: any;
+    };
+
+    interface QueryResultsInterface<T> {
+        Items: T[];
+        DataScannedInMB: number;
+        QueryCostInUSD: number;
+        EngineExecutionTimeInMillis: number;
+        Count: number;
+    }
+
+    interface QueryInterface {
+        sql: string;
+        db: string;
+    }
+
+    type QueryResult = QueryResultsInterface<any>;
+    type QueryFunc = (query: QueryInterface) => Promise<QueryResult>;
+    interface AthenaExpressInterface {
+        new: (config: ConnectionConfigInterface) => any;
+        query: QueryFunc;
+    }
+
+    class AthenaExpress {
+        new: (config: ConnectionConfigInterface) => any;
+        constructor(config: ConnectionConfigInterface);
+        query: QueryFunc;
+    }
+}
+
